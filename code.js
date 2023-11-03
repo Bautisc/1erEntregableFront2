7a0370d3fa575d961e30e30168f84a6c41866e19
Bautista Sanguinetti <bautiscsc.123@gmail.com>
2023-11-02 21:36:31 -0300

    arreglando cagadas v1

--- code.js
+++ code.js
@@ -35,33 +35,74 @@ cambiarTema.addEventListener("click", alternarColorTema);
 
 function obtenerDatosDelUsuario() {
   /* --------------- PUNTO 1: Escribe tu codigo a partir de aqui --------------- */
- 
+
+    do {
+        datosPersona.nombre = prompt("Ingresa tu nombre de usuario:")
+    } while (datosPersona.nombre === null || datosPersona.nombre == "" || datosPersona.nombre.length < 3);
 
 
+    do {
+        fechaNacimiento = parseInt(prompt("Ingresa tu fecha de nacimiento:"))
+        datosPersona.edad = 2023 - fechaNacimiento 
+    } while (!fechaNacimiento || fechaNacimiento.isNaN);
+
+    do {
+        datosPersona.ciudad = prompt("Ingresa el nombre tu Ciudad ")
+    } while (datosPersona.ciudad === null || datosPersona.ciudad == "");
+
+    leInteresaJs = confirm("Te interesa Javascript?") 
+    if (leInteresaJs) {
+        datosPersona.interesPorJs = "Si" 
+    }else {
+        datosPersona.interesPorJs = "No"
+    }
 }
 
 function renderizarDatosUsuario() {
   /* ------------------- NO TOCAR NI ELIMINAR ESTA FUNCION. ------------------- */
   obtenerDatosDelUsuario();
   /* --------------- PUNTO 2: Escribe tu codigo a partir de aqui --------------- */
-  
 
+    const nombre= document.querySelector("#nombre");
+    nombre.textContent = datosPersona.nombre
+
+    const edad = document.querySelector("#edad");
+    edad.textContent = datosPersona.edad
+
+    const ciudad = document.querySelector("#ciudad");
+    ciudad.textContent = datosPersona.ciudad
+
+    const interesPorJs = document.querySelector("#javascript");
+    interesPorJs.textContent = datosPersona.interesPorJs
 
 }
 
-
 function recorrerListadoYRenderizarTarjetas() {
   /* ------------------ PUNTO 3: Escribe tu codigo desde aqui ------------------ */
-  
+    const mostrarMaterias = document.querySelector("#fila");
+
+    mostrarMaterias.innerHTML = ""
+    console.log(mostrarMaterias)
+
+    listado.forEach(lenguaje => {
+        const div = document.createElement("div")
+        div.setAttribute("class", "caja")
+
+        const img = document.createElement("img")
+        img.setAttribute("src", listado.imgUrl)
+        img.setAttribute("alt", listado.lenguajes)
 
 
+        const pBimestre = document.createElement("p")
+        const pLenguajes = document.createElement("p")
+    })
 }
 
 function alternarColorTema() {
   /* --------------------- PUNTO 4: Escribe tu codigo aqui --------------------- */
  
-  
-
+  let oscuro = document.querySelector("#sitio")
+    oscuro.classList.toggle("dark");
 
 }
 
